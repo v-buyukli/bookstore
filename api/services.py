15a8c7527b1230310a0a449005dc202177b0348a
Viktor <119663044+v-buyukli@users.noreply.github.com>
2023-07-14 16:50:44 +0300

    fix token (#11)

--- api/services.py
+++ api/services.py
@@ -6,16 +6,10 @@ from django.conf import settings
 
 def get_access_token():
     conn = http.client.HTTPSConnection(settings.AUTH0_DOMAIN)
-    payload_dict = {
-        "client_id": {settings.AUTH0_CLIENT_ID},
-        "client_secret": {settings.AUTH0_CLIENT_SECRET},
-        "audience": "https://bookstore/api",
-        "grant_type": "client_credentials",
-    }
-    payload = str(payload_dict)
+    payload = '{"client_id":"FMG1uFUNlBEeCc0xu8mhHDeHx5P56fsi","client_secret":"V4xf2hnmoYqt-4HNu0psJ98b6W-GEzYf7lpu264ksN-XCGUCCYu807uUIqmQxZLT","audience":"https://bookstore/api","grant_type":"client_credentials"}'
     headers = {"content-type": "application/json"}
     conn.request("POST", "/oauth/token", payload, headers)
     res = conn.getresponse()
     data = res.read()
-    token_data = json.loads(data.decode("utf-8"))
+    token_data = json.loads(data)
     return token_data["access_token"]
