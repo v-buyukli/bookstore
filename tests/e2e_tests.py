3a61d71e2ef6097008106ed19b660fe9283fe8c2
viktor <vktr.buyukli@gmail.com>
2023-07-22 17:44:09 +0300

    fix e2e

--- tests/e2e_tests.py
+++ tests/e2e_tests.py
@@ -131,12 +131,13 @@ def test_get_order_info():
     assert response_data["id"] == order_id
 
 
-def test_create_order():
-    order_data = {"order": [{"book_id": 598, "quantity": 5}]}
+def test_create_order(created_book):
+    response = created_book
+    book_id = response.json().get("id")
+    order_data = {"order": [{"book_id": book_id, "quantity": 3}]}
 
     response = requests.post(f"{BASE_URL}/order/", json=order_data)
     response_data = response.json()
-
     assert "url" in response_data
     assert "id" in response_data
     assert isinstance(response_data["url"], str)
